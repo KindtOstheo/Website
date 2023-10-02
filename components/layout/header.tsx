d9e55bd8c4c349647042fb51db65e114f6f064c9
BlackWolfie <lucvdv83@hotmail.fr>
2023-10-02 08:36:13 +0200

    correct navbar

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -7,6 +7,20 @@ import { useTheme } from ".";
 import { Icon } from "../util/icon";
 import { tinaField } from "tinacms/dist/react";
 import { GlobalHeader } from "../../tina/__generated__/types";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition  } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export const Header = ({ data }: { data: GlobalHeader }) => {
   const router = useRouter();
@@ -19,147 +33,238 @@ export const Header = ({ data }: { data: GlobalHeader }) => {
     },
   };
 
-  const headerColor = {
-    default:
-      "text-black dark:text-white from-gray-50 to-white dark:from-gray-800 dark:to-gray-900",
-    primary: {
-      blue: "text-white from-blue-300 to-blue-500",
-      teal: "text-white from-teal-400 to-teal-500",
-      green: "text-white from-green-400 to-green-500",
-      red: "text-white from-red-400 to-red-500",
-      pink: "text-white from-pink-400 to-pink-500",
-      purple: "text-white from-purple-400 to-purple-500",
-      orange: "text-white from-orange-400 to-orange-500",
-      yellow: "text-white from-yellow-400 to-yellow-500",
-    },
-  };
-
-  const activeItemClasses = {
-    blue: "border-b-3 border-blue-200 text-blue-700 dark:text-blue-300 font-medium dark:border-blue-700",
-    teal: "border-b-3 border-teal-200 text-teal-700 dark:text-teal-300 font-medium dark:border-teal-700",
-    green:
-      "border-b-3 border-green-200 text-green-700 dark:text-green-300 font-medium dark:border-green-700",
-    red: "border-b-3 border-red-300 text-red-700 dark:text-green-300 font-medium dark:border-red-700",
-    pink: "border-b-3 border-pink-200 text-pink-700 dark:text-pink-300 font-medium dark:border-pink-700",
-    purple:
-      "border-b-3 border-purple-200 text-purple-700 dark:text-purple-300 font-medium dark:border-purple-700",
-    orange:
-      "border-b-3 border-orange-200 text-orange-700 dark:text-orange-300 font-medium dark:border-orange-700",
-    yellow:
-      "border-b-3 border-yellow-300 text-yellow-700 dark:text-yellow-300 font-medium dark:border-yellow-600",
-  };
-
-  const activeBackgroundClasses = {
-    blue: "text-blue-500",
-    teal: "text-teal-500",
-    green: "text-green-500",
-    red: "text-red-500",
-    pink: "text-pink-500",
-    purple: "text-purple-500",
-    orange: "text-orange-500",
-    yellow: "text-yellow-500",
-  };
   const [isClient, setIsClient] = React.useState(false);
   React.useEffect(() => {
     setIsClient(true);
   }, []);
-
   return (
-    <div
-      className={`relative overflow-hidden`}
-      style={Styles.color}
-    >
-      <Container size="custom" className="py-0 relative z-10 max-w-8xl">
-        <div className="flex items-center justify-between gap-6">
-          <h4 className="select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform">
-            <Link
-              href="/"
-              className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]"
-            >
-            {data.icon.name ?
-              <Icon
-                tinaField={tinaField(data, "icon")}
-                parentColor={data.color}
-                data={{
-                  name: data.icon.name,
-                  color: data.icon.color,
-                  style: data.icon.style,
-                }}
-              /> :
-              <Image
-                  src={data.icon.image ? data.icon.image : ""}
-                  width={80}
-                  height={80}
-                  data-tina-field={tinaField(data.icon, 'image')} alt={""} />
-            }
-          </Link>
-          </h4>
-          <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] -mx-4">
-            {data.nav &&
+    <Disclosure as="nav" className="relative overflow-hidden" style={Styles.color}>
+      {({ open }) => (
+        <>
+          <div className="mx-auto my-4 max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#8d6e5e] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link
+                    href="/"
+                    className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]"
+                  >
+                    {data.icon.name ?
+                      <Icon
+                        tinaField={tinaField(data, "icon")}
+                        parentColor={data.color}
+                        data={{
+                          name: data.icon.name,
+                          color: data.icon.color,
+                          style: data.icon.style,
+                        }}
+                      /> :
+                      <Image
+                          src={data.icon.image ? data.icon.image : ""}
+                          width={80}
+                          height={80}
+                          data-tina-field={tinaField(data.icon, 'image')} alt={""} />
+                    }
+                  </Link>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                  <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] -mx-4">
+                  {data.nav &&
+                    data.nav.map((item, i) => {
+                      const activeItem =
+                      (item.href === "" ? router.asPath === "/" : router.asPath.includes(item.href)) && isClient;
+                      return (
+                    <li
+                      key={`${item.label}-${i}`}
+                    >
+                      <Link
+                        data-tina-field={tinaField(item, "label")}
+                        href={`/${item.href}`}
+                        className={`relative select-none	text-base inline-block tracking-wide transition duration-150 ease-out hover:text-[#9e6851] py-8 px-4 ${
+                          activeItem ? `text-[#8f6e5d]` : ``
+                        }`}
+                      >
+                        {item.label}
+                        {activeItem && (
+                          <svg
+                            className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2  opacity-10 dark:opacity-15`}
+                            preserveAspectRatio="none"
+                            viewBox="0 0 230 230"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <rect
+                              x="230"
+                              y="230"
+                              width="230"
+                              height="230"
+                              transform="rotate(-180 230 230)"
+                              fill="url(#paint0_radial_1_33)"
+                            />
+                            <defs>
+                              <radialGradient
+                                id="paint0_radial_1_33"
+                                cx="0"
+                                cy="0"
+                                r="1"
+                                gradientUnits="userSpaceOnUse"
+                                gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+                              >
+                                <stop stopColor="currentColor" />
+                                <stop
+                                  offset="1"
+                                  stopColor="currentColor"
+                                  stopOpacity="0"
+                                />
+                              </radialGradient>
+                            </defs>
+                          </svg>
+                        )}
+                      </Link>
+                    </li>
+                    )})}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {data.nav &&
               data.nav.map((item, i) => {
                 const activeItem =
-                  (item.href === ""
-                    ? router.asPath === "/"
-                    : router.asPath.includes(item.href)) && isClient;
+                (item.href === "" ? router.asPath === "/" : router.asPath.includes(item.href)) && isClient;
                 return (
-                  <li
-                    key={`${item.label}-${i}`}
-                  >
-                    <Link
-                      data-tina-field={tinaField(item, "label")}
-                      href={`/${item.href}`}
-                      className={`relative select-none	text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-8 px-4 ${
-                        activeItem ? `text-[#8f6e5d]` : ``
-                      }`}
-                    >
-                      {item.label}
-                      {activeItem && (
-                        <svg
-                          className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15`}
-                          preserveAspectRatio="none"
-                          viewBox="0 0 230 230"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <rect
-                            x="230"
-                            y="230"
-                            width="230"
-                            height="230"
-                            transform="rotate(-180 230 230)"
-                            fill="url(#paint0_radial_1_33)"
-                          />
-                          <defs>
-                            <radialGradient
-                              id="paint0_radial_1_33"
-                              cx="0"
-                              cy="0"
-                              r="1"
-                              gradientUnits="userSpaceOnUse"
-                              gradientTransform="translate(345 230) rotate(90) scale(230 115)"
-                            >
-                              <stop stopColor="currentColor" />
-                              <stop
-                                offset="1"
-                                stopColor="currentColor"
-                                stopOpacity="0"
-                              />
-                            </radialGradient>
-                          </defs>
-                        </svg>
-                      )}
-                    </Link>
-                  </li>
-                );
-              })}
-          </ul>
-        </div>
-        <div
-          className={`absolute h-1 bg-gradient-to-r from-transparent ${
-            data.color === "primary" ? `via-white` : `via-black dark:via-white`
-          } to-transparent bottom-0 left-4 right-4 -z-1 opacity-5`}
-        />
-      </Container>
-    </div>
-  );
+                <Disclosure.Button
+                  key={`${item.label}-${i}`}
+                  as="a"
+                  href={`/${item.href}`}
+                  className={classNames(
+                    activeItem ? 'text-[#8f6e5d] hover:bg-[#a7a6a6]' : ' hover:bg-[#8d6e5e] hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={activeItem  ? 'page' : undefined}
+                >
+                  {item.label}
+                </Disclosure.Button>
+              )})}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+  // return (
+  //   <div
+  //     className={`relative overflow-hidden`}
+  //     style={Styles.color}
+  //   >
+  //     <Container size="custom" className="py-0 relative z-10 max-w-8xl">
+  //       <div className="flex items-center justify-between gap-6">
+  //         <h4 className="select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform">
+  //           <Link
+  //             href="/"
+  //             className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]"
+  //           >
+  //           {data.icon.name ?
+  //             <Icon
+  //               tinaField={tinaField(data, "icon")}
+  //               parentColor={data.color}
+  //               data={{
+  //                 name: data.icon.name,
+  //                 color: data.icon.color,
+  //                 style: data.icon.style,
+  //               }}
+  //             /> :
+  //             <Image
+  //                 src={data.icon.image ? data.icon.image : ""}
+  //                 width={80}
+  //                 height={80}
+  //                 data-tina-field={tinaField(data.icon, 'image')} alt={""} />
+  //           }
+  //         </Link>
+  //         </h4>
+  //         <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] -mx-4">
+  //           {data.nav &&
+  //             data.nav.map((item, i) => {
+  //               const activeItem =
+  //                 (item.href === ""
+  //                   ? router.asPath === "/"
+  //                   : router.asPath.includes(item.href)) && isClient;
+  //               return (
+  //                 <li
+  //                   key={`${item.label}-${i}`}
+  //                 >
+  //                   <Link
+  //                     data-tina-field={tinaField(item, "label")}
+  //                     href={`/${item.href}`}
+  //                     className={`relative select-none	text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-8 px-4 ${
+  //                       activeItem ? `text-[#8f6e5d]` : ``
+  //                     }`}
+  //                   >
+  //                     {item.label}
+  //                     {activeItem && (
+  //                       <svg
+  //                         className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15`}
+  //                         preserveAspectRatio="none"
+  //                         viewBox="0 0 230 230"
+  //                         fill="none"
+  //                         xmlns="http://www.w3.org/2000/svg"
+  //                       >
+  //                         <rect
+  //                           x="230"
+  //                           y="230"
+  //                           width="230"
+  //                           height="230"
+  //                           transform="rotate(-180 230 230)"
+  //                           fill="url(#paint0_radial_1_33)"
+  //                         />
+  //                         <defs>
+  //                           <radialGradient
+  //                             id="paint0_radial_1_33"
+  //                             cx="0"
+  //                             cy="0"
+  //                             r="1"
+  //                             gradientUnits="userSpaceOnUse"
+  //                             gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+  //                           >
+  //                             <stop stopColor="currentColor" />
+  //                             <stop
+  //                               offset="1"
+  //                               stopColor="currentColor"
+  //                               stopOpacity="0"
+  //                             />
+  //                           </radialGradient>
+  //                         </defs>
+  //                       </svg>
+  //                     )}
+  //                   </Link>
+  //                 </li>
+  //               );
+  //             })}
+  //         </ul>
+  //       </div>
+  //       <div
+  //         className={`absolute h-1 bg-gradient-to-r from-transparent ${
+  //           data.color === "primary" ? `via-white` : `via-black dark:via-white`
+  //         } to-transparent bottom-0 left-4 right-4 -z-1 opacity-5`}
+  //       />
+  //     </Container>
+  //   </div>
+  // );
 };
