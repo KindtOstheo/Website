225ea51d8ab133699a3b17dbc25882883cfbb37d
BlackWolfie <luc.vandevelde@viacesi.fr>
2023-11-08 10:44:24 +0100

    correct config

--- tina/config.tsx
+++ tina/config.tsx
@@ -362,6 +362,10 @@ const config = defineConfig({
         format: "json",
         ui: {
           global: true,
+          allowedActions : {
+            create: false,
+            delete: false
+          }
         },
         fields: [
           {
